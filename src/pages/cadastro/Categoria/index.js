4767e694eccb97c4b98aa4da787c39caf250760a
lucas00023 <correia23.lucas@gmail.com>
2020-07-29 15:52:48 -0300

    :sparkles: page categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,12 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import Button from '../../../components/Button';
+import FormField from '../../../components/FormField';
 
-function cadastroCategoria() {
+function CadastroCategoria() {
+
+    const [categorias, setCategorias] = useState([])
+    const [value, setValue] = useState({
+        name: "",
+        description: "",
+        color: "#000"
+    });
+
+    function handleChange(key, newValue) {
+        console.log("atualizou")
+        setValue({
+            ...value,
+            [key]: newValue
+        });
+    }
+
+    function handleCadastro(e) {
+        e.preventDefault();
+        setCategorias([
+            ...categorias,
+            value
+        ]);
+        setValue({
+            name: "",
+            description: "",
+            color: "#000"
+        });
+    }
+    function handleDelete(e, pos) {
+        e.preventDefault();
+        const listLocal = [...categorias];
+        listLocal.splice(pos, 1);
+
+        setCategorias(listLocal);
+    }
     return (
         <>
             <PageDefault>
-                <h1>Cadastro de Categoria</h1>
+                <h1>Cadastro de Categoria:  {value.name}</h1>
+
+                <form onSubmit={handleCadastro}>
+
+                    <FormField
+                        label="Nome da Categoria"
+                        type="text"
+                        value={value.name}
+                        placeholder="digite a categoria"
+                        onChange={(event) => {
+                            handleChange('name', event.target.value);
+                        }}
+                    />
+
+                    <FormField
+                        label="Descrição"
+                        type="text"
+                        value={value.description}
+                        placeholder="digite a  descrição da categoria"
+                        onChange={(event) => {
+                            handleChange('description', event.target.value);
+                        }}
+                    />
+
+                    <FormField
+                        label="Cor"
+                        type="color"
+                        value={value.color}
+                        onChange={(event) => {
+                            handleChange('color', event.target.value);
+                        }}
+                    />
+
+                    <ul>
+                        {categorias.map((value, indice) => {
+                            return <li key={indice}>{value.name} | {value.description} | {value.color} <span> | <Button as="a" style={{ "padding": "5px 24px" }} onClick={(e) => { handleDelete(e, indice) }}>remover</Button></span></li>
+                        })}
+                    </ul>
+
+                    <button>
+                        Cadastrar
+                    </button>
+                </form>
 
                 <Link to="/">
                     ir para home
@@ -16,4 +95,4 @@ function cadastroCategoria() {
     );
 }
 
-export default cadastroCategoria;
+export default CadastroCategoria;
